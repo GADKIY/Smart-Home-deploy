28e22f4ea5a92cfbb6e6a8fa2fdec6b742bf6ce3
Anton Samoilenko <antonplane@gmail.com>
2021-07-15 18:42:25 +0300

    rebuild project with style fixes

--- precache-manifest.2cea6125c3168eeb4cda86726c27e7e5.js
+++ precache-manifest.2cea6125c3168eeb4cda86726c27e7e5.js
@@ -1,51 +1,51 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a302dbd0c64467460e58",
+    "revision": "d6c650c3f3ea4a0cae34",
     "url": "css/app.09c418ac.css"
   },
   {
-    "revision": "85bd287eee7b7fcd3087",
+    "revision": "93e0329e3fceece42ce7",
     "url": "css/chunk-vendors.902ebb66.css"
   },
   {
-    "revision": "66819e80b6a8c4e71b46",
+    "revision": "68e3335930c1c34a8f31",
     "url": "css/layout-components-modal-vue.2cd37f8c.css"
   },
   {
-    "revision": "db9ff0c7bde3939cca44",
+    "revision": "4a4c6b7c90ff1c249921",
     "url": "css/layout-components-room-vue.a2ef6f22.css"
   },
   {
-    "revision": "73dab7914c63ec846ab8",
+    "revision": "7d6a01c33b5914ff212d",
     "url": "css/layout-layouts-auth-vue.3805db56.css"
   },
   {
-    "revision": "4db86eb03eecd60742da",
+    "revision": "b05696b0da8ef716386c",
     "url": "css/layout-layouts-default-vue.7386cdea.css"
   },
   {
-    "revision": "608c1ebc4daf1ff331e0",
+    "revision": "ec3e60363868d80ff950",
     "url": "css/layout-views-login-vue.04fa0319.css"
   },
   {
-    "revision": "ad3a2a2e61fec2b0aad8",
-    "url": "css/layout-views-member-vue.fc65f258.css"
+    "revision": "291883f4926233dd1e73",
+    "url": "css/layout-views-member-vue.9d606d2d.css"
   },
   {
-    "revision": "7a26c481a54c3795a8b7",
-    "url": "css/layout-views-members-vue.01081d78.css"
+    "revision": "ea80ea6af109f9f5533e",
+    "url": "css/layout-views-members-vue.10b89309.css"
   },
   {
-    "revision": "77a99ee94df6706c7b1a",
-    "url": "css/layout-views-rooms-vue.f0f2c677.css"
+    "revision": "c4be7f341761198da64d",
+    "url": "css/layout-views-rooms-vue.da899dd7.css"
   },
   {
-    "revision": "332a6d23e501a5965d80",
-    "url": "css/layout-views-statistics-vue.fdc00816.css"
+    "revision": "55bd555581bed8381ada",
+    "url": "css/layout-views-statistics-vue.818794c7.css"
   },
   {
-    "revision": "5b21fed14fa5b47f6460",
-    "url": "css/layout-views-utilityBilling-vue.4d506dcb.css"
+    "revision": "34991fac6d449a43c388",
+    "url": "css/layout-views-utilityBilling-vue.7d7584c6.css"
   },
   {
     "revision": "b6fd3078b336ba2978467caf144ec934",
@@ -152,68 +152,68 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "img/sprite.d1c1c928.svg"
   },
   {
-    "revision": "12320eb0cb17c43c67bd8e32fb9fec64",
+    "revision": "cf1ff4c0ac084ec385e3cda3923d8b54",
     "url": "index.html"
   },
   {
-    "revision": "a302dbd0c64467460e58",
-    "url": "js/app.e40cfc75.js"
+    "revision": "d6c650c3f3ea4a0cae34",
+    "url": "js/app.0a64fdcd.js"
   },
   {
     "revision": "ea95050ebf423ada01a8",
     "url": "js/canvg.b16d9a45.js"
   },
   {
-    "revision": "85bd287eee7b7fcd3087",
-    "url": "js/chunk-vendors.f6606097.js"
+    "revision": "93e0329e3fceece42ce7",
+    "url": "js/chunk-vendors.b29aad9a.js"
   },
   {
-    "revision": "66819e80b6a8c4e71b46",
-    "url": "js/layout-components-modal-vue.7a416a90.js"
+    "revision": "68e3335930c1c34a8f31",
+    "url": "js/layout-components-modal-vue.effdc5a1.js"
   },
   {
-    "revision": "db9ff0c7bde3939cca44",
-    "url": "js/layout-components-room-vue.d4fd773a.js"
+    "revision": "4a4c6b7c90ff1c249921",
+    "url": "js/layout-components-room-vue.003c4712.js"
   },
   {
-    "revision": "73dab7914c63ec846ab8",
-    "url": "js/layout-layouts-auth-vue.c47a9229.js"
+    "revision": "7d6a01c33b5914ff212d",
+    "url": "js/layout-layouts-auth-vue.1476a447.js"
   },
   {
-    "revision": "4db86eb03eecd60742da",
-    "url": "js/layout-layouts-default-vue.5e4332dc.js"
+    "revision": "b05696b0da8ef716386c",
+    "url": "js/layout-layouts-default-vue.d5dfb4d7.js"
   },
   {
     "revision": "95803d2455da23823347",
     "url": "js/layout-layouts-default-vue~layout-views-login-vue~layout-views-member-vue~layout-views-members-vue~l~5c914e80.a211d5c9.js"
   },
   {
-    "revision": "608c1ebc4daf1ff331e0",
-    "url": "js/layout-views-login-vue.c6f558f2.js"
+    "revision": "ec3e60363868d80ff950",
+    "url": "js/layout-views-login-vue.a03ec318.js"
   },
   {
-    "revision": "ad3a2a2e61fec2b0aad8",
-    "url": "js/layout-views-member-vue.595e3728.js"
+    "revision": "291883f4926233dd1e73",
+    "url": "js/layout-views-member-vue.e09b5511.js"
   },
   {
-    "revision": "7a26c481a54c3795a8b7",
-    "url": "js/layout-views-members-vue.b781e88e.js"
+    "revision": "ea80ea6af109f9f5533e",
+    "url": "js/layout-views-members-vue.ace17cd4.js"
   },
   {
-    "revision": "77a99ee94df6706c7b1a",
-    "url": "js/layout-views-rooms-vue.8b219228.js"
+    "revision": "c4be7f341761198da64d",
+    "url": "js/layout-views-rooms-vue.7ec41994.js"
   },
   {
-    "revision": "332a6d23e501a5965d80",
-    "url": "js/layout-views-statistics-vue.f30b782f.js"
+    "revision": "55bd555581bed8381ada",
+    "url": "js/layout-views-statistics-vue.388cf02b.js"
   },
   {
     "revision": "540066a81e336491843b",
     "url": "js/layout-views-statistics-vue~layout-views-utilityBilling-vue.eb474214.js"
   },
   {
-    "revision": "5b21fed14fa5b47f6460",
-    "url": "js/layout-views-utilityBilling-vue.dbfbaeaf.js"
+    "revision": "34991fac6d449a43c388",
+    "url": "js/layout-views-utilityBilling-vue.d46469fc.js"
   },
   {
     "revision": "8474591c920c7d5cb2d6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9a71cfc7acd3aafa270abad17416fd12.js"
+  "precache-manifest.2cea6125c3168eeb4cda86726c27e7e5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "hsmart"});
